b716ef3d6e3c265763bc6fb44404dc5ab2879f9a
Joe Fredette <jfredett@gmail.com>
2014-04-03 23:58:00 -0400

    execute commands individually in .box rule

--- lib/rules.rb
+++ lib/rules.rb
@@ -1,10 +1,9 @@
 rule %r{boxes/arch-.*\.box} => 'boxes/arch-base.box' do |t|
   t.name =~ /arch-([^\.]*).box/
   box = $1
-  sh %Q(
-    vagrant destroy -f arch-#{box}
-    vagrant --parallel up arch-#{box}
-    vagrant package --base arch-#{box} --output boxes/arch-#{box}.box
-    vagrant box add arch-#{box}-local boxes/arch-#{box}.box
-  )
+  sh %Q(vagrant destroy -f arch-#{box})
+  sh %Q(vagrant up arch-#{box})
+  sh %Q{rm -f boxes/arch-#{box}.box}
+  sh %Q(vagrant package arch-#{box} --output boxes/arch-#{box}.box)
+  sh %Q(vagrant box add arch-#{box}-local boxes/arch-#{box}.box)
 end
