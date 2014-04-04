cfb508e0f225cc4fe2f20a92f57adc48805a89d1
Joe Fredette <jfredett@gmail.com>
2014-04-04 00:30:08 -0400

    remove obsoleted rake targets

--- lib/rules.rb
+++
@@ -1,9 +0,0 @@
-rule %r{boxes/arch-.*\.box} => 'boxes/arch-base.box' do |t|
-  t.name =~ /arch-([^\.]*).box/
-  box = $1
-  sh %Q(vagrant destroy -f arch-#{box})
-  sh %Q(vagrant up arch-#{box})
-  sh %Q{rm -f boxes/arch-#{box}.box}
-  sh %Q(vagrant package arch-#{box} --output boxes/arch-#{box}.box)
-  sh %Q(vagrant box add arch-#{box}-local boxes/arch-#{box}.box)
-end
