a1fceba1412084dea371333ec1755aefb4450f7d
Joe Fredette <jfredett@gmail.com>
2014-04-08 01:42:43 -0400

    refactor upload rake script, add upload:all target

--- lib/upload.rb
+++ lib/upload.rb
@@ -1,17 +1,19 @@
 namespace :upload do
   task :chef => 'boxes/arch-chef.box' do
-    Rake::Task[:"upload:go"].invoke("chef")
+    Rake::Task[:'upload:go'].invoke('chef')
   end
 
   task :puppet => 'boxes/arch-puppet.box' do
-    Rake::Task[:"upload:go"].invoke("puppet")
+    Rake::Task[:'upload:go'].invoke('puppet')
   end
 
   task :base => 'boxes/arch-base.box' do
-    Rake::Task[:"upload:go"].invoke("base")
+    Rake::Task[:'upload:go'].invoke('base')
   end
 
   task :go, [:box] do |_, args|
+    puts "uploading #{args[:box]}"
+
     s3 = AWS::S3.new
     bucket = s3.buckets['jgf-vagrantboxes']
 
@@ -21,7 +23,11 @@ namespace :upload do
 
     obj = bucket.objects["archbox/v#{version}/arch-#{box}.box"]
     obj.write(file: "boxes/arch-#{box}.box")
+
+    puts "finished uploading #{args[:box]}"
   end
+
+  task :all => ['upload[base]', 'upload[chef]', 'upload[puppet]']
 end
 
 desc 'upload the given box to S3'
@@ -30,4 +36,3 @@ task :upload, [:box] do |_, args|
   # the chef box in one go.
   Rake::Task[:"upload:#{args[:box]}"].invoke
 end
-
