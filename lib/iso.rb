fe75e22adbdbea31cb06070887bfc9e5055a9f9d
Joe Fredette <jfredett@gmail.com>
2014-04-18 09:46:02 -0400

    fix checksum in arch-base to match the 2014.04.01 iso

--- lib/iso.rb
+++ lib/iso.rb
@@ -2,19 +2,22 @@ require 'rubygems'
 
 namespace :iso do
   def arch
-    "#{CONFIG['iso']['mirror']}/#{CONFIG['date']}/archlinux-#{CONFIG['date']}-dual"
+    "#{CONFIG['iso']['mirror']}/#{CONFIG['iso']['date']}/archlinux-#{CONFIG['iso']['date']}-dual"
   end
 
   directory 'templates'
 
+  NUKE.include('templates/base.iso')
   file 'templates/base.iso' => 'templates' do
     sh "wget #{arch}.iso -O templates/base.iso"
   end
 
+  NUKE.include('templates/base.iso.sig')
   file 'templates/base.iso.sig' => 'templates' do
     sh "wget #{arch}.iso.sig -O templates/base.iso.sig"
   end
 
+  NUKE.include('templates/verified')
   file 'templates/verified' => ['templates/base.iso', 'templates/base.iso.sig'] do
     sh 'gpg --verify templates/base.iso.sig templates/base.iso > templates/verified'
   end
