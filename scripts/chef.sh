c1d418c7a5a0473d5052b727e4b7fca61ddef9c2
Joe Fredette <jfredett@gmail.com>
2014-08-17 14:09:20 -0400

    use chef-dk AUR instead of omnibus chef

--- scripts/chef.sh
+++ scripts/chef.sh
@@ -5,9 +5,4 @@ export PATH=/usr/local/bin:$PATH
 
 sudo pacman -S --noconfirm ruby git base-devel
 aur-install --noconfirm -i ruby-bundler
-sudo aur-install --asroot --noconfirm -f omnibus-chef-git
-
-# set up /etc/profile w/ the fixed PATH
-cp /etc/profile /tmp/profile
-echo 'export "PATH=/opt/chef/bin:$PATH"' >> /tmp/profile
-sudo mv /tmp/profile /etc/profile
+aur-install --noconfirm -i chef-dk
